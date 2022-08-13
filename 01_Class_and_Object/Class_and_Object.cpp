4b52055c90907bc8c94968700371dd09df265c6d
“Sagnik Mandal” <sagnikma@gmail.com>
2022-08-14 00:29:42 +0530

    Class and Object

---
+++ 01_Class_and_Object/Class_and_Object.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Rectangle {
+    private:
+        int length,breadth;
+
+    public:
+        int area(int l, int b) {
+            length = l;
+            breadth = b;
+            return length * breadth; 
+        }
+};
+
+int main() {
+    Rectangle Obj1,Obj2;
+    cout<<Obj1.area(5,10)<<'\n';
+    cout<<Obj2.area(3,4);
+
+}
